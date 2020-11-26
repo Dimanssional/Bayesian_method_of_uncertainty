488123320f5b6b02d249584ca583bf372ab917d6
root <dimalagunov6@gmail.com>
2020-11-27 00:02:26 +0100

    main changing

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import pandas as pd
+from Bayes_model import BayesModel
 
-KNB = pd.DataFrame(pd.read_csv("KB_1.csv", delimiter=";"))
+KNB = pd.DataFrame(pd.read_csv("KNB2.csv", delimiter=","))
+
+pd.set_option("display.max_rows", None, "display.max_columns", None)
+
+BayesianModel = BayesModel(KNB)
+KNB = BayesianModel.data_correct()
+print(KNB)
+
+print(BayesianModel.compute_prior())
+print(BayesianModel.compute_ctr())
+print(KNB)
+print(BayesianModel.compute_glob())
+print(BayesianModel.get_O())
 print(KNB)
