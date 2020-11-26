438adbe5f585ad25c3d55a2f42ef6915b6d46e67
root <dimalagunov6@gmail.com>
2020-11-26 12:26:07 +0100

    "load data"

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import pandas as pd
 
+KNB = pd.DataFrame(pd.read_csv("KB_1.csv", delimiter=";"))
+pd.set_option("display.max_rows", None, "display.max_columns", None)
 
-print("Hello Bayes")
+print(KNB)
 
